83b9bfc73953daef9cefc927aa38cb165999616b
mayachos <amacrk@icloud.com>
2021-07-17 20:05:23 +0900

    エラーの変更 コメントの【変更】部分に注目

--- Timer/SceneDelegate.swift
+++ Timer/SceneDelegate.swift
@@ -7,10 +7,19 @@
 
 import UIKit
 
+//デリゲート用の変数、関数
+protocol backgroundTimerDelegate: class {
+    func setCurrentTimer(_ elapsedTime:Float)
+    func deleteTimer()
+    func checkBackground()
+    var timerIsBackground:Bool { set get }
+}
+
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    weak var delegate: backgroundTimerDelegate?
+    let ud = UserDefaults.standard
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
@@ -29,11 +38,23 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        if delegate?.timerIsBackground == true {
+                    let calender = Calendar(identifier: .gregorian)
+                    let date1 = ud.value(forKey: "date1") as! Date
+                    let date2 = Date()
+                    let elapsedTime = calender.dateComponents([.second], from: date1, to: date2).second!
+            delegate?.setCurrentTimer(Float(elapsedTime))
+                }
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
+        func sceneWillResignActive(_ scene: UIScene) {
+                ud.set(Date(), forKey: "date1")
+                delegate?.checkBackground()
+                delegate?.deleteTimer()
+            }
     }
 
     func sceneWillEnterForeground(_ scene: UIScene) {
--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -7,13 +7,114 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, backgroundTimerDelegate {
+    
+    var timerIsBackground: Bool = false
+    
+    var judge: Bool = false
+    @IBOutlet var label: UILabel!
+    
+    
+    //【変更】Int型からFloat型に変更
+    var count: Float = 0.00
+    
+    var timer: Timer = Timer()
+    var saveData: UserDefaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @objc func up() {
+        
+        //【変更】countを0.01足す
+        count = count + 0.01
+        
+        //ラベルに小数点以下2桁まで表示
+        label.text = String(format: "%.2f", count)
+        }
+        
+        @IBAction func start() {
+            
+            if !timer.isValid {
+                
+                //タイマーが動作してなかったら動かす
+                //【変更】timeIntervalを0.01に変更
+                timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(self.up), userInfo: nil, repeats: true)
+                judge = true
+
+            }
+        }
+        
+       
+        
+        @IBAction func stop() {
+            if timer.isValid {
+                //タイマーが動作していたら停止する
+                timer.invalidate()
+                judge = false
+            }
+        }
+        
+        // ViewController.swift
+        @IBAction func nextButtonTouchUp(_ sender: UIButton) {
+            let nextVC = storyboard?.instantiateViewController(identifier: "HomeView")
+
+            nextVC?.modalPresentationStyle = .fullScreen // ★この行追加
+            self.present(nextVC!, animated: true, completion: nil)
+        }
+       
+        @IBAction func reset() {
+            if timer.isValid {
+                timer.invalidate()
+            }
+            count = 0
+            label.text = String(format: "%.2f", count)
+        }
+        func checkBackground() {
+            if judge == true {
+                
+                //バックグラウンドへの移行を確認
+                //【変更】条件式をlet _ = timerからtimer.isValidへ変更
+                if timer.isValid {
+                    timerIsBackground = true
+                }
+            }
+        }
+        
+    //【変更】elapsedTimeの型をIntからFloatに変更
+        func setCurrentTimer(_ elapsedTime:Float) {
+            if judge == true {
+                //残り時間から引数(バックグラウンドでの経過時間)を足す
+                count += elapsedTime
+                label.text = "\(count)"
+                
+                //【変更】timeIntervalを0.01に変更
+                //再びタイマーを起動
+                timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(self.up), userInfo: nil, repeats: true)
+            }
+        }
+        
+        func deleteTimer() {
+            if judge == true {
+                
+                //起動中のタイマーを破棄
+                //【変更】条件式をlet _ = timerからtimer.isValidへ変更
+                if timer.isValid {
+                    timer.invalidate()
+                }
+            }
+        }
+        
+        
+
+        
+
+        @IBAction func saveMemo() {
+            //UserDefaultsに書き込み
+            saveData.set(label.text, forKey: "time")
+        }
 
 }
 
